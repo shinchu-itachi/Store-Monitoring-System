dba39a0864c25deb0a2130d20c00536f0d3feb7a
Niteesh Kumar <88903346+shinchu-itachi@users.noreply.github.com>
2023-09-22 14:55:45 +0530

    Update config.py

--- Store-Monitoring-System/config.py
+++ Store-Monitoring-System/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///restaurant_monitoring.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///store_monitoring.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
